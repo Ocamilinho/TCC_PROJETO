5ebf2d08308a955fd3c68b4a3f3147b3ffa08f79
Adriano Camilo <adrianocamilo055@gmail.com>
2023-03-16 17:57:28 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php include "includes/conexao"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SigaB</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href=""></a></li>
+            </ul>
+            <ul>
+                <li><a href=""></a></li>
+            </ul>
+            <ul>
+                <li><a href=""></a></li>
+            </ul>   
+        </nav>'
+    </header>
+    <script src="js/script.js"></script>
+    <footer>
+        
+    </footer>
+</body>
+</html>
