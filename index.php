5d245d4a6ad8171bccf28c09486393312dc8ad09
Adriano Camilo <adrianocamilo055@gmail.com>
2023-03-18 12:12:44 -0300

    segundo commit

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-<?php include "includes/conexao"?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,15 +11,12 @@
         <nav>
             <ul>
                 <li><a href=""></a></li>
-            </ul>
-            <ul>
                 <li><a href=""></a></li>
-            </ul>
-            <ul>
                 <li><a href=""></a></li>
             </ul>   
-        </nav>'
+        </nav>
     </header>
+    <main>
     <script src="js/script.js"></script>
     <footer>
         
